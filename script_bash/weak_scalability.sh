ab9342cbc03f8ef3be7a4c714825df9259fb7019
findSaul0 <antoniofasulo69@gmail.com>
2022-06-26 16:54:21 +0200

    ws_script_bash

--- script_bash/weak_scalability.sh
+++ script_bash/weak_scalability.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 
-rm -rf results/strong_scalability.csv
+rm -rf results/weak_scalability.csv
 
 mpicc -c -g main.c utils.c
 mpicc -o word_count main.o utils.o
 
-echo "TIME,VCPU,FILES,WORDS" >> results/strong_scalability.csv
+echo "TIME,VCPU,FILES,WORDS" >> results/weak_scalability.csv
 
 
 for ((p=1; p<=24; p=p+1))
 do
-    mpirun --allow-run-as-root -np $p --hostfile hostfile word_count $p >> results/strong_scalability.csv
+    mpirun --allow-run-as-root -np $p --hostfile hostfile word_count $p >> results/weak_scalability.csv
 done
 
-cat results/strong_scalability.csv
+cat results/weak_scalability.csv
